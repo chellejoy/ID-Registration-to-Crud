3ad3eb1d7afeb9c55c1dcbc1b7bf835279da59d5
Richelle Joy Catabian <richellecatabian2gmail.com>
2022-11-14 16:22:14 -0800

    id-registration to crud

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Welcome!</h2>
+	<p>Welcome to the University of the Cordilleras!</p>
+	<p>Complete your data and be part of the UCians Family!</p>
+	<p>YesUCan!</p>
+</div>
+
+<?=template_footer()?>
